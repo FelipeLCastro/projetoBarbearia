cf886840ec20658de2c1181dab9e685641ec7c45
FelipeLCastro <92058820+FelipeLCastro@users.noreply.github.com>
2025-06-18 14:16:33 -0300

    Create PlanoAssinaturaRepository.java

---
+++ PlanoAssinaturaRepository.java
@@ -0,0 +1,7 @@
+package com.barbearia.assinatura.repository;
+
+import com.barbearia.assinatura.model.PlanoAssinatura;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PlanoAssinaturaRepository extends JpaRepository<PlanoAssinatura, Long> {
+}
